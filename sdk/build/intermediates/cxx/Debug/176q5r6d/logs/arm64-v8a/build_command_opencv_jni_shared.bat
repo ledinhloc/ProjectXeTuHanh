3d79bcbe7a60cdb961930791bc62e164f0a760d4
ledinhloc <ledinhloc7@gmail.com>
2025-05-11 23:43:18 +0700

    update

--- sdk/build/intermediates/cxx/Debug/176q5r6d/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ sdk/build/intermediates/cxx/Debug/176q5r6d/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\ASUS TUF\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "D:\\he_thong_nhung\\project_xe\\ProjectXeTuHanh\\sdk\\.cxx\\Debug\\176q5r6d\\arm64-v8a" ^
+  "D:\\he_thong_nhung\\ProjectXeTuHanh\\sdk\\.cxx\\Debug\\176q5r6d\\arm64-v8a" ^
   opencv_jni_shared
